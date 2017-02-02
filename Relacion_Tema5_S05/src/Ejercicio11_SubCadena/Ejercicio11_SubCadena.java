ae61ed8162198e1ce11583c5f5858ea12529bb06
1dam.m <1dam.m@PC112.cenecmalaga.net>
2017-02-02 10:32:43 +0100

    easy

--- Relacion_Tema5_S05/src/Ejercicio11_SubCadena/Ejercicio11_SubCadena.java
+++ Relacion_Tema5_S05/src/Ejercicio11_SubCadena/Ejercicio11_SubCadena.java
@@ -4,28 +4,27 @@ public class Ejercicio11_SubCadena {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String cadena = "La moto me la robaron";
-		String subcadena = "La moto";
+		String cadena = "La moto me la robaron, hecho de menos mi moto";
+		String subcadena = "moto";
 
 		System.out.println("La nueva cadena de '" + cadena + "' sería: "
 				+ subCadena(cadena, subcadena));
 	}
 
-	public static String subCadena(String cadena, String subcadena) {
-		int posicion = cadena.indexOf(subcadena);
-		char cadenita[] = cadena.toCharArray();
-		String cadenanueva = "";
-
-		if (posicion >= 0) {
-			cadenita[posicion] = '*';
-			for (int i = 0; i < cadenita.length; i++) {
-				cadenanueva = cadenanueva + cadenita[i];
-			}
-		} else {
-			cadenanueva = "\nERROR: No se encontro la subcadena";
+	public static String subCadena(String cadena, String subcadena){
+		String subcadena2 = "";
+		String cadena2 = "";
+		char sub [] = subcadena.toCharArray();
+		
+		sub[0] = '*';
+		
+		for(int i=0; i < sub.length; i++){
+			subcadena2 = subcadena2 + sub[i];
 		}
-
-		return cadenanueva;
+		
+		cadena2 = cadena.replaceAll(subcadena, subcadena2);
+		
+		return cadena2;
 	}
 
 }
